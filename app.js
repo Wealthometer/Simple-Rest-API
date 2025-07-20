d6d5228000b329029c35644494d4ddea9a193be5
Wealthometer <wealthhajoh87@gmail.com>
2025-07-20 12:12:26 +0100

    Update

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }))
+app.use(express.urlencoded({ extended: true }));
 
-const routes = require('./server/routes/movieRoutes.js')
+const routes = require('./server/routes/movieRoutes.js');
 app.use('/', routes);
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
